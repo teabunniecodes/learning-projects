f4ef9314359d1dc6e89739550a24718162297351
teabunniecodes <97705887+teabunniecodes@users.noreply.github.com>
2022-03-30 23:42:39 -0700

    Update linked_list.py

--- linked_list.py
+++ linked_list.py
@@ -4,9 +4,8 @@
 # file, read in each line as a separate linked list, merge the \
 # two lists together, then print the output.
 
-
-
 # Personal Projects/Text Files/number_list.txt
+# Python/Self Projects/Text Files/number_list.txt 
 file = input("Path of text file: ")
 
 with open(file) as read:
@@ -16,18 +15,28 @@ with open(file) as read:
     read.close()
 
 print(original)
+length = len(original)
+print(length)
 
 x = 0
-y = 1
+y = 0
+z = 1
+list_ = {}
+
 for int in original:
     list = original[x]
     split_list = list.split(", ")
-    list = dict()
-    for y in range(len(original)):
-        list[y] = split_list
+    if y < length:
         y += 1
-        print(list)
+        list_[y] = split_list
+        print(f"list_{y}: {list_[y]}")
     x += 1
     print(split_list)
 
-
+if z < length:
+    merge_list = list_[z]
+    z += 1
+    merge_list = merge_list + list_[z]
+    print(f"Merged List: {merge_list}")
+    sort_list = merge_list.sort()
+    print(f"Sorted list: {sort_list}")
