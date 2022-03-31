9b98b9f02d1dc589980c1684fc863f4088cfc6a8
teabunniecodes <97705887+teabunniecodes@users.noreply.github.com>
2022-03-30 18:13:18 -0700

    Update linked_list.py

--- linked_list.py
+++ linked_list.py
@@ -6,7 +6,7 @@
 
 
 
-# file path - Personal Projects/Text Files/number_list.txt
+# Personal Projects/Text Files/number_list.txt
 file = input("Path of text file: ")
 
 with open(file) as read:
@@ -18,10 +18,16 @@ with open(file) as read:
 print(original)
 
 x = 0
+y = 1
 for int in original:
     list = original[x]
-    print(list.split())
-    x +=1
-    #combined = original[x-1] - original [x]
+    split_list = list.split(", ")
+    list = dict()
+    for y in range(len(original)):
+        list[y] = split_list
+        y += 1
+        print(list)
+    x += 1
+    print(split_list)
 
-print()
+
